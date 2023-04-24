2db64e9163ec834aa1b02ea992c166d0a22277ff
Mateusz Siatrak <siatrak.mateusz@gmail.com>
2023-04-24 19:07:19 +0100

    Initial Commit

---
+++ HRangeBar/HRangeBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  HRangeBarApp.swift
+//  HRangeBar
+//
+//  Created by Mateusz Siatrak on 24/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct HRangeBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
